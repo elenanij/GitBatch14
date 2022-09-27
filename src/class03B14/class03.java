361e29965612c20530bf3ae1e2204e5b2a4d18f5
elenanij <elenanij@gmail.com>
2022-09-27 16:52:22 -0700

    second commit

--- src/class03B14/class03.java
+++ src/class03B14/class03.java
@@ -4,9 +4,9 @@ public class class03 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		System.out.println("hey this is a git class");
 
+		System.out.println("hey this is a git class");
+		System.out.println("hey this is a change in my project");
 	}
 
 }
